e1866c2382d89b9e142c7aa13e98459479402193
Getafix <thierrydna@me.com>
2022-08-09 13:45:51 +1000

    first django install

---
+++ listmaster/listmaster/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from listmaster.users.models import User
+from listmaster.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
